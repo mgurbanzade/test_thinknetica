a77b9e1c21c8aeebc18bd363f5e489c88f925d5e
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-01-22 20:59:25 +0400

    Area of triangle

---
+++ Ruby_Basics_part_1/area_of_triangle.rb
@@ -0,0 +1,7 @@
+puts "Укажите основание треугольника:"
+base = gets.chomp.to_i
+
+puts "Укажите высоту треугольника:"
+height = gets.chomp.to_i
+
+puts "Площадь треугольника составляет #{base * height / 2.0}."
