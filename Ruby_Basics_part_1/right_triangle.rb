658287971f037e4e01b74a8b744ac5ed10525136
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-01-22 23:15:42 +0400

    right triangle

---
+++ Ruby_Basics_part_1/right_triangle.rb
@@ -0,0 +1,23 @@
+puts "Поочередно укажите значения сторон треугольника:"
+
+sides = [gets.chomp.to_i, gets.chomp.to_i, gets.chomp.to_i]
+sides = sides.sort
+
+if sides[0] == sides[1] && sides[0] == sides[2]
+  puts "Треугольник не является прямоугольным. Он равнобедренный и равносторонний"
+end
+
+cathethus_a = sides[0]
+cathethus_b = sides[1]
+hypotenuse = sides[2]
+
+
+if (cathethus_a ** 2 + cathethus_b ** 2) == hypotenuse ** 2
+  puts "Треугольник является прямоугольным."
+
+elsif ((cathethus_a ** 2 + cathethus_b ** 2) * 2) == hypotenuse ** 2
+  puts "Треугольник является прямоугольным и равнобедренным."
+
+else
+  puts "Треугольник не является прямоугольным."
+end
