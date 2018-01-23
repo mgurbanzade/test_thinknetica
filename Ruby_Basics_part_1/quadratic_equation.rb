382a34db660413f75fadf0494d53afd80f81f730
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-01-23 13:35:08 +0400

    Update quadratic equation

--- Ruby_Basics_part_1/quadratic_equation.rb
+++ Ruby_Basics_part_1/quadratic_equation.rb
@@ -4,18 +4,18 @@ a = gets.chomp.to_f
 b = gets.chomp.to_f
 c = gets.chomp.to_f
 d = (b ** 2) - (4 * a * c)
-d_root = Math.sqrt(d)
-
-x1 = (-b + d_root) / 2 * a
-x2 = (-b - d_root) / 2 * a
 
 if d < 0
   puts "Дискриминант: #{d}, корней нет"
 
 elsif d == 0
-  puts "Дискриминант: #{d}, корень: #{x1}"
+  x = (-b / (2 * a))
+  puts "Дискриминант: #{d}, корень: #{x}"
 
 else
+  d_root = Math.sqrt(d)
+  x1 = (-b + d_root) / (2 * a)
+  x2 = (-b - d_root) / (2 * a)
   puts "Дискриминант: #{d}, корни: #{x1}, #{x2}"
 end
 
