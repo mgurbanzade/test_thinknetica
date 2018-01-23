a4260fb766262437409212254db03560b92b617f
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-01-23 12:03:50 +0400

    code optimization

--- Ruby_Basics_part_1/quadratic_equation.rb
+++ Ruby_Basics_part_1/quadratic_equation.rb
@@ -3,19 +3,19 @@ puts "Последовательно введите 3 числа"
 a = gets.chomp.to_f
 b = gets.chomp.to_f
 c = gets.chomp.to_f
-
 d = (b ** 2) - (4 * a * c)
+d_root = Math.sqrt(d)
+
+x1 = (-b + d_root) / 2 * a
+x2 = (-b - d_root) / 2 * a
 
 if d < 0
   puts "Дискриминант: #{d}, корней нет"
-else
-  x1 = (-b + Math.sqrt(d)) / 2 * a
-  x2 = (-b - Math.sqrt(d)) / 2 * a
 
-  if d == 0
-    puts "Дискриминант: #{d}, корень: #{x1}"
-  else
-    puts "Дискриминант: #{d}, корни: #{x1}, #{x2}"
-  end
+elsif d == 0
+  puts "Дискриминант: #{d}, корень: #{x1}"
+
+else
+  puts "Дискриминант: #{d}, корни: #{x1}, #{x2}"
 end
 
