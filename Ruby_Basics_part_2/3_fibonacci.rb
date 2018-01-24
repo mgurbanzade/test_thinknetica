4de5a5a8410f005488f0181454cb0274203d375c
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-01-24 20:13:32 +0400

    code opt

--- Ruby_Basics_part_2/3_fibonacci.rb
+++ Ruby_Basics_part_2/3_fibonacci.rb
@@ -1,9 +1,11 @@
 i = 2
 fibonacci = [0, 1]
 
-while fibonacci.last < 89 do
+while fibonacci.last < 100 do
   fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
   i += 1
 end
+
+fibonacci.pop
 
 puts fibonacci
--- Ruby_Basics_part_2/4_alphabet.rb
+++ Ruby_Basics_part_2/4_alphabet.rb
@@ -1,8 +1,8 @@
 array_alphabet = ('A'..'Z').to_a
 hash_alphabet = {}
 
-array_alphabet.each_with_index do |value, index|
-  hash_alphabet["#{value}"] = index + 1 if value.match(/A|E|I|O|U/)
+array_alphabet.each.with_index(1) do |value, index|
+  hash_alphabet["#{value}"] = index if value.match(/A|E|I|O|U/)
 end
 
 puts hash_alphabet
