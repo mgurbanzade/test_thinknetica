a8120a95435f7ee20ace7b283899ce66b68c4275
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-01-24 20:50:26 +0400

    upd code

--- Ruby_Basics_part_2/3_fibonacci.rb
+++ Ruby_Basics_part_2/3_fibonacci.rb
@@ -1,11 +1,8 @@
-i = 2
 fibonacci = [0, 1]
 
-while fibonacci.last < 100 do
-  fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
-  i += 1
+while fibonacci[-1] < 100 do
+  fibonacci << fibonacci[-1] + fibonacci[-2]
 end
 
 fibonacci.pop
-
 puts fibonacci
