4ad087c25dfbd07ef7a347937ff12fe2231d8174
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-02-04 12:16:25 +0400

    Code upd ver 3.0

--- Class_Methods/cargo_train.rb
+++ Class_Methods/cargo_train.rb
@@ -1,11 +1,6 @@
 require_relative 'train'
 
 class CargoTrain < Train
-
-  def initialize(number)
-    super
-  end
-
   def type
     :cargo
   end
--- Class_Methods/passenger_train.rb
+++ Class_Methods/passenger_train.rb
@@ -1,11 +1,6 @@
 require_relative 'train'
 
 class PassengerTrain < Train
-
-  def initialize(number)
-    super
-  end
-
   def type
     :passenger
   end
