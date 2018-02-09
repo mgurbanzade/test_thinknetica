f8676f0d7538a4e92b274b50e5a0e4d14199f75d
Mahram Gurbanzade <mgurbanzade@yahoo.com[diff]>
2018-02-09 13:04:32 +0400

    upd code ver 2.0

--- Blocks_Proc_Lambda/cargo_wagon.rb
+++ Blocks_Proc_Lambda/cargo_wagon.rb
@@ -1,11 +1,8 @@
 require_relative 'wagon'
 
 class CargoWagon < Wagon
-  attr_reader :total_capacity
-
   def initialize(args)
     @type = :cargo
-    @total_capacity = args
     super
   end
 end
--- Blocks_Proc_Lambda/passenger_wagon.rb
+++ Blocks_Proc_Lambda/passenger_wagon.rb
@@ -1,11 +1,8 @@
 require_relative 'wagon'
 
 class PassengerWagon < Wagon
-  attr_reader :total_seats
-
   def initialize(args)
     @type = :passenger
-    @total_seats = args
     super
   end
 
